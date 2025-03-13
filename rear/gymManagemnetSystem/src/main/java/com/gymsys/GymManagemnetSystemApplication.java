3b425590ce21fd5a2db5a7e2c9fb4453013a4aef
ccc212 <2120496201@qq.com>
2025-03-13 15:09:56 +0800

    项目初始化

---
+++ rear/gymManagemnetSystem/src/main/java/com/gymsys/GymManagemnetSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gymsys.gymmanagemnetsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymManagemnetSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymManagemnetSystemApplication.class, args);
+    }
+
+}
---
+++ rear/gymManagemnetSystem/src/test/java/com/gymsys/GymManagemnetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymsys.gymmanagemnetsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymManagemnetSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

c7538aff670b87f05d2c6f80abc67abe557a34b0
Geek <byizhg@gamil.com>
2025-03-14 18:57:50 +0800

    1.0 项目初始化，设置包名

--- rear/gymManagemnetSystem/src/main/java/com/gymsys/GymManagemnetSystemApplication.java
+++ rear/gymManagemnetSystem/src/main/java/com/gymsys/GymManagemnetSystemApplication.java
@@ -1,4 +1,4 @@
-package com.gymsys.gymmanagemnetsystem;
+package com.gymsys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
